4dc3cd160756ae52e438edce76a2b1435ec8001c
KeithS <keith.sparks2@gmail.com>
2014-03-29 21:27:04 -0400

    Added errors through packages

--- collections/posts.js
+++ collections/posts.js
@@ -1,4 +1,14 @@
 Posts = new Meteor.Collection('posts');
+Posts.allow({
+  update: ownsDocument,
+  remove: ownsDocument
+});
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    //may only edit the following two fields:
+    return (_.without(fieldnames, 'url', 'title').length > 0);
+  }
+});
 
 Meteor.methods({
   post: function(postAttributes) {
@@ -19,14 +29,23 @@ Meteor.methods({
       }
 
       // pick out the whitelisted keys
-      var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+      var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
+        title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
         userId: user._id,
         author: user.username,
         submitted: new Date().getTime()
       });
 
+      // wait for 5 seconds
+      if (! this.isSimulation) {
+        var Future = Npm.require('fibers/future');
+        var future = new Future();
+        Meteor.setTimeout(function() {
+          future.return();
+        }, 5 * 1000);
+        future.wait();
+      }
       var postId = Posts.insert(post);
-
       return postId;
   }
 });
